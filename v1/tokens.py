e0537133c8fa99479706770a8d5973fbc5274cc6
scott.a.idler <scott.a.idler@gmail.com>
2016-06-23 00:08:23 -0700

    reordered line and pos first for easier reading... -sai

--- v1/tokens.py
+++ v1/tokens.py
@@ -3,25 +3,25 @@
 import re
 
 class Token(object):
-    def __init__(self, kind, line, pos, value):
-        self.kind = kind if kind is not True else value
+    def __init__(self, line, pos, kind, value):
         self.line = line
         self.pos = pos
+        self.kind = kind if kind is not True else value
         self.value = value
 
     def __str__(self):
-        return '[kind=%s line=%d pos=%d value=%s]' % (
-            self.kind,
+        return '[line=%d pos=%d kind=%s value=%s]' % (
             self.line,
             self.pos,
+            self.kind,
             self.value)
 
     def __repr__(self):
         fmt = 'Token(%s, %s, %s, %s)'
         return fmt % (
-            self.kind,
             self.line,
             self.pos,
+            self.kind,
             self.value)
 
 #    def is_kind(self, *kinds):
